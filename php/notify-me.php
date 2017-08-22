2afef073bc487c32e8df87a59ef5860ede1d805f
Suraj R <rsuraj38@gmail.com>
2017-08-22 23:09:16 +0530

    Store email ids in file

--- php/notify-me.php
+++ php/notify-me.php
@@ -5,7 +5,7 @@
 /* ******************************************************** */
 
 // Set to "mailchimp" or "file"
-$STORE_MODE = "mailchimp";
+$STORE_MODE = "file";
 
 // MailChimp API Key findable in your Mailchimp's dashboard
 $API_KEY =  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX-XX0";
